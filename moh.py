d2cd43167941382d7ae58f0872fdb2cfd117caa3
lateraTesfyae <61625870+lateraTesfyae@users.noreply.github.com>
2021-10-02 16:43:06 +0300

    Revert "New_Update"
    This reverts commit f19b0f33746fa7d5cf05a4a4b547c710f223ed3c.

--- moh.py
+++ moh.py
@@ -86,8 +86,6 @@ from sklearn import linear_model
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 import matplotlib.pyplot as plt
-import seaborn as sb
-from matplotlib import style
 from sklearn.preprocessing import PolynomialFeatures
 # print(predictedU5_a)
 col0 = st.sidebar
@@ -102,13 +100,8 @@ with col0:
         df_w = pandas.read_csv("wasting.csv")
         df_u = pandas.read_csv("underweight.csv")
         df_a = pandas.read_csv("anemia.csv")
-        # style.use('seaborn-whitegrid')
-        # plt.rcParams['figure.figsize'] = (20, 10)
-        # sb.pairplot(df)
-        # plt.savefig('pairplor.png')
-        # plt.show()
+        X = df[['EI', 'CF', 'EBF', 'YCA', 'YCA_MMF', 'FBF', 'ASF', 'ISH', 'FV', 'FP', 'IWS', 'WOP_L30', 'HH_BSF','IRF', 'VAF', 'WL', 'ISF', 'ISW']]
 
-        X = df[['EI', 'CF', 'YCA',  'FBF', 'ASF', 'FV', 'FP', 'IWS', 'WOP_L30', 'HH_BSF','IRF', 'VAF', 'WL', 'ISF', 'ISW']]
         y = df['Stunting']
         y_w = df_w['Wasting']
         y_u = df_u['Underweight']
@@ -129,89 +122,11 @@ with col0:
         lin2.fit(X_poly, y)
         lin2_w.fit(X_poly, y_w)
         lin2_u.fit(X_poly, y_u)
-        var_list_stunting = ['EI', 'CF', 'EBF', 'YCA', 'YCA_MMF', 'FBF', 'ASF', 'ISH', 'FV', 'FP', 'IWS', 'WOP_L30', 'HH_BSF','IRF', 'VAF', 'WL', 'ISF', 'ISW','Stunting']
-        var_list_wasting = ['EI', 'CF', 'EBF', 'YCA', 'YCA_MMF', 'FBF', 'ASF', 'ISH', 'FV', 'FP', 'IWS', 'WOP_L30', 'HH_BSF','IRF', 'VAF', 'WL', 'ISF', 'ISW','Wasting']
-        var_list_underweight = ['EI', 'CF', 'EBF', 'YCA', 'YCA_MMF', 'FBF', 'ASF', 'ISH', 'FV', 'FP', 'IWS', 'WOP_L30', 'HH_BSF','IRF', 'VAF', 'WL', 'ISF', 'ISW','Underweight']
-
-        importance_stunting = lin2.coef_
-        importance_wasting = lin2_w.coef_
-        imprortance_underweight = lin2_u.coef_
-        var_big_stunting = {}
-        var_big_wasting = {}
-        var_big_underweight = {}
-        # summarize feature importance
-        for i, v in enumerate(importance_stunting):
-            var_big_stunting[var_list_stunting[i]] = abs(v)
-            print('Feature: %0d, Score_stunting: %.5f' % (i, v))
-        # plot feature importance
-        # plt.bar([x for x in range(len(importance_stunting))], importance_stunting)
-        # plt.show()
-        for i, v in enumerate(importance_wasting):
-            var_big_wasting[var_list_wasting[i]] = abs(v)
-            print('Feature: %0d, Score_wasting: %.5f' % (i, v))
-        # plot feature importance
-        # plt.bar([x for x in range(len(importance_wasting))],  importance_wasting)
-        # plt.show()
-        for i, v in enumerate(imprortance_underweight):
-            var_big_underweight[var_list_underweight[i]] = abs(v)
-            print('Feature: %0d, Score_underweight: %.5f' % (i, v))
-        # plot feature importance
-        # plt.bar([x for x in range(len(imprortance_underweight))],  imprortance_underweight)
-        # plt.show()
         # lin2_a.fit(X_poly, y_a)
-        sort_orders_stunting = dict(sorted(var_big_stunting.items(), key=lambda x: x[1], reverse=True))
-        sort_orders_wasting = dict(sorted(var_big_wasting.items(), key=lambda x: x[1], reverse=True))
-        sort_orders_underweight = dict(sorted(var_big_underweight.items(), key=lambda x: x[1], reverse=True))
-        print("Stunting")
-        print(sort_orders_stunting)
-        print("Wasting")
-        print(sort_orders_wasting)
-        print("Under-weight")
-        print(sort_orders_underweight)
-        names = list(sort_orders_stunting.keys())
-        values = list(sort_orders_stunting.values())
-
-        plt.bar(range(len(sort_orders_stunting)), values, tick_label=names)
-        plt.xticks(rotation=90)
-        plt.show()
-        names = list(sort_orders_wasting.keys())
-        values = list(sort_orders_wasting.values())
-
-        plt.bar(range(len(sort_orders_wasting)), values, tick_label=names)
-        plt.xticks(rotation=90)
-        plt.show()
-        names = list(sort_orders_underweight.keys())
-        values = list(sort_orders_underweight.values())
-
-        plt.bar(range(len(sort_orders_underweight)), values, tick_label=names)
-        plt.xticks(rotation=90)
-        plt.show()
         print("results")
-        #2024 p1
-        # predictedU5 = lin2.predict(poly.fit_transform([[82.0,80.0,77.0,50.0,65.0,17.0,23.1,93.3,35.0,38.0,29.0,87.5,26.2,40.0,55.0,59.6,39.5,75.0]]))
-        # predictedU5_w = lin2_w.predict(poly_w.fit_transform([[82.0,80.0,77.0,50.0,65.0,17.0,23.1,93.3,35.0,38.0,29.0,87.5,26.2,40.0,55.0,59.6,39.5,75.0]]))
-        # predictedU5_u = lin2_u.predict(poly_u.fit_transform([[82.0,80.0,77.0,50.0,65.0,17.0,23.1,93.3,35.0,38.0,29.0,87.5,26.2,40.0,55.0,59.6,39.5,75.0]]))
-
-        # #2024 p2
-        # predictedU5 = lin2.predict(poly.fit_transform([[84.0,85.0,80.0,52.0,67.0,17.0,31.0,93.3,43.0,46.0,37.0,89.0,34.2,48.0,63.0,63.6,47.5,77.0]]))
-        # predictedU5_w = lin2_w.predict(poly_w.fit_transform([[84.0,85.0,80.0,52.0,67.0,17.0,31.0,93.3,43.0,46.0,37.0,89.0,34.2,48.0,63.0,63.6,47.5,77.0]]))
-        # predictedU5_u = lin2_u.predict(poly_u.fit_transform([[84.0,85.0,80.0,52.0,67.0,17.0,31.0,93.3,43.0,46.0,37.0,89.0,34.2,48.0,63.0,63.6,47.5,77.0]]))
-
-        # # 2030 p3
-        # predictedU5 = lin2.predict(poly.fit_transform([[100.0,98.0,95.0,75.0,90.0,35.0,73.1,100.0,85.0,82.0,73.0,100.0,64.2,90.0,100.0,83.6,82.5,100.0]]))
-        # predictedU5_w = lin2_w.predict(poly_w.fit_transform([[100.0,98.0,95.0,75.0,90.0,35.0,73.1,100.0,85.0,82.0,73.0,100.0,64.2,90.0,100.0,83.6,82.5,100.0]]))
-        # predictedU5_u = lin2_u.predict(poly_u.fit_transform([[100.0,98.0,95.0,75.0,90.0,35.0,73.1,100.0,85.0,82.0,73.0,100.0,64.2,90.0,100.0,83.6,82.5,100.0]]))
-
-        # # 2030 p2
-        # predictedU5 = lin2.predict(poly.fit_transform([[90.0,90.0,85.0,70.0,85.0,35.0,53.1,100.0,65.0,62.0,53.0,97.0,46.2,70.0,85.0,74.6,64.5,90.0]]))
-        # predictedU5_w = lin2_w.predict(poly_w.fit_transform([[90.0,90.0,85.0,70.0,85.0,35.0,53.1,100.0,65.0,62.0,53.0,97.0,46.2,70.0,85.0,74.6,64.5,90.0]]))
-        # predictedU5_u = lin2_u.predict(poly_u.fit_transform([[90.0,90.0,85.0,70.0,85.0,35.0,53.1,100.0,65.0,62.0,53.0,97.0,46.2,70.0,85.0,74.6,64.5,90.0]]))
-
-        # 2030 p1
-        predictedU5 = lin2.predict(poly.fit_transform([[86.0,87.0,83.0,60.0,70.0,25.0,40.0,93.3,50.0,53.0,43.0,91.0,37.2,55.0,70.0,65.6,52.5,81.0]]))
-        predictedU5_w = lin2_w.predict(poly_w.fit_transform([[86.0,87.0,83.0,60.0,70.0,25.0,40.0,93.3,50.0,53.0,43.0,91.0,37.2,55.0,70.0,65.6,52.5,81.0]]))
-        predictedU5_u = lin2_u.predict(poly_u.fit_transform([[86.0,87.0,83.0,60.0,70.0,25.0,40.0,93.3,50.0,53.0,43.0,91.0,37.2,55.0,70.0,65.6,52.5,81.0]]))
-
+        predictedU5 = lin2.predict(poly.fit_transform([[100, 98, 95, 75, 90, 35, 73.1, 95, 85, 82, 73,100, 100, 90, 100, 100, 100, 100]]))
+        predictedU5_w = lin2_w.predict(poly_w.fit_transform([[100, 98, 95, 75, 90, 35, 73.1, 95, 85, 82, 73,100, 100,  90, 100, 100, 100, 100]]))
+        predictedU5_u = lin2_u.predict(poly_u.fit_transform([[100, 98, 95, 75, 90, 35, 73.1, 95, 85, 82, 73,100, 100,  90, 100, 100, 100, 100]]))
         # predictedU5_a = lin2_a.predict(poly_a.fit_transform([[72.00,69.00,58.80,13.50,55.10,4.80,3.90,89.30,18.20,28.80,19.80,56.90,10.20,53.40,24.20,39.40,47.60]]))
         print(predictedU5)
         print(predictedU5_w)
@@ -219,15 +134,15 @@ with col0:
         with col1:
             number3 = st.slider("Ealy Initiation(EI)", 47.4, 100.0)
             number4 = st.slider("Complimentary Food(CF)", 36.5, 100.0)
-            # number5 = st.slider("Exclusively Breastfeding(EBF)", 49.0, 100.0)
+            number5 = st.slider("Exclusively Breastfeding(EBF)", 49.0, 100.0)
             number6 = st.slider("Children 6-23 months fed 5+ food groups(5+FG)", 1.0, 100.0)
-            # number7 = st.slider("Minimum Meal Frequency (MMF)", 41.8, 100.0)
+            number7 = st.slider("Minimum Meal Frequency (MMF)", 41.8, 100.0)
             number8 = st.slider("Fortified baby food (FBF)", 3.1, 100.0)
             number9 = st.slider("Children Receiving Meat, Fish, Poultry(ASF)", 0.0, 100.0)
             number10 = st.slider("Improved Sanitation Facility(ISF)", 0.6, 100.0)
             number1 = st.slider("Iron Supplement for Women(ISW)", 4.0, 100.0)
         with col2:
-            # number11 = st.slider("Iodized Salt in Household(ISIHH)", 25.5, 100.0)
+            number11 = st.slider("Iodized Salt in Household(ISIHH)", 25.5, 100.0)
             number12 = st.slider("Fully Vaccinated(FV)", 6.4, 100.0)
             number13 = st.slider("Family Planning(FP)", 5.9, 100.0)
             number14 = st.slider("Households with an Improved Water Source(IWS) ", 4.7, 100.0)
@@ -241,16 +156,16 @@ with col0:
             import altair as alt
             import pandas as pd
 
-            predictedU5 = lin2.predict(poly.fit_transform([[number3, number4, number6,  number8,
-                                                            number9,  number12, number13, number14, number15,
+            predictedU5 = lin2.predict(poly.fit_transform([[number3, number4, number5, number6, number7, number8,
+                                                            number9, number11, number12, number13, number14, number15,
                                                             number16,  number18, number19, number20, number10,
                                                             number1]]))
-            predictedU5_w = lin2_w.predict(poly_w.fit_transform([[number3, number4,  number6,  number8,
-                                                                  number9,  number12, number13, number14,
+            predictedU5_w = lin2_w.predict(poly_w.fit_transform([[number3, number4, number5, number6, number7, number8,
+                                                                  number9, number11, number12, number13, number14,
                                                                   number15, number16,  number18, number19,
                                                                   number20, number10, number1]]))
-            predictedU5_u = lin2_u.predict(poly_u.fit_transform([[number3, number4,  number6, number8,
-                                                                  number9,  number12, number13, number14,
+            predictedU5_u = lin2_u.predict(poly_u.fit_transform([[number3, number4, number5, number6, number7, number8,
+                                                                  number9, number11, number12, number13, number14,
                                                                   number15, number16, number18, number19,
                                                                   number20, number10, number1]]))
             if predictedU5[0] <= 0:
@@ -430,7 +345,6 @@ with col0:
         lin2.fit(X_poly, y)
         lin2_w.fit(X_poly, y_w)
         lin2_u.fit(X_poly, y_u)
-
         # lin2_a.fit(X_poly, y_a)
         predictedU5 = lin2.predict(poly.fit_transform([[73.40, 20.20, 63.50, 87.70, 38.90, 26.80, 39.90, 69.00, 14.20,
                                                         19.10, 36.10, 52.70, 59.60, 33.00, 85.60]]))
